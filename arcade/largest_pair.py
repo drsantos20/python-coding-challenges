d914cf58dfeb34c5838e9610c3db813bc7655a05
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-05-09 00:26:49 -0300

    Add cover test for largest number

--- arcade/largest_pair.py
+++ arcade/largest_pair.py
@@ -1,11 +1,12 @@
 
 class LargestPair:
     def adjacent_elements_product(input_array):
-        largest_pair = 0
+        largest_pair = -999
         for i in range(len(input_array)-1):
-            import pdb; pdb.set_trace()
             sum_of_pairs = input_array[i] * input_array[i + 1]
-            if largest_pair > sum_of_pairs:
+            print(sum_of_pairs)
+            import pdb; pdb.set_trace()
+            if sum_of_pairs > largest_pair:
                 largest_pair = sum_of_pairs
 
         return largest_pair
--- tests/unit/largest_pair_test.py
+++ tests/unit/largest_pair_test.py
@@ -10,3 +10,8 @@ class TestLargestPair(unittest.TestCase):
 
     def test_largest_pair(self):
         self.assertEqual(21, self.test_largest_pair.adjacent_elements_product([3, 6, -2, -5, 7, 3]))
+        self.assertEqual(2, self.test_largest_pair.adjacent_elements_product([-1, -2]))
+        self.assertEqual(6, self.test_largest_pair.adjacent_elements_product([5, 1, 2, 3, 1, 4]))
+        self.assertEqual(6, self.test_largest_pair.adjacent_elements_product([1, 2, 3, 0]))
+        self.assertEqual(50, self.test_largest_pair.adjacent_elements_product([9, 5, 10, 2, 24, -1, -48]))
+        self.assertEqual(-12, self.test_largest_pair.adjacent_elements_product([-23, 4, -3, 8, -12]))
