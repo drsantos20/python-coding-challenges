6a0aec52fbfb1dde18f106106ca3ae10d3d46992
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-05-09 23:44:56 -0300

    Refactor test directory

---
+++ test/unit/test_largest_pair.py
@@ -0,0 +1,17 @@
+import unittest
+
+from arcade.largest_pair import LargestPair
+
+
+class TestLargestPair(unittest.TestCase):
+
+    def setUp(self):
+        self.largest_pair = LargestPair()
+
+    def test_largest_pair(self):
+        self.assertEqual(21, self.largest_pair.adjacent_elements_product([3, 6, -2, -5, 7, 3]))
+        self.assertEqual(2, self.largest_pair.adjacent_elements_product([-1, -2]))
+        self.assertEqual(6, self.largest_pair.adjacent_elements_product([5, 1, 2, 3, 1, 4]))
+        self.assertEqual(6, self.largest_pair.adjacent_elements_product([1, 2, 3, 0]))
+        self.assertEqual(50, self.largest_pair.adjacent_elements_product([9, 5, 10, 2, 24, -1, -48]))
+        self.assertEqual(-12, self.largest_pair.adjacent_elements_product([-23, 4, -3, 8, -12]))
---
+++ test/unit/test_palindrome.py
@@ -0,0 +1,17 @@
+import unittest
+
+from arcade.palindrome_challenge import Palindrome
+
+
+class TestPalindrome(unittest.TestCase):
+
+    def setUp(self):
+        self.palindrome = Palindrome()
+
+    def test_palindrome(self):
+        self.assertTrue(self.palindrome.check_palindrome('a'))
+        self.assertTrue(self.palindrome.check_palindrome('aabaa'))
+        self.assertTrue(self.palindrome.check_palindrome('aacbcaa'))
+        self.assertFalse(self.palindrome.check_palindrome('ab'))
+        self.assertFalse(self.palindrome.check_palindrome('aaxbcaa'))
+        self.assertFalse(self.palindrome.check_palindrome('aabcaabc'))
